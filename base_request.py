a1877ca6460ab4b4423f9b6a7329a724b1529b95
GeniyIT <45914940+GeniyIT@users.noreply.github.com>
2023-12-27 19:02:29 +0300

    /

--- base_request.py
+++ base_request.py
@@ -1,44 +1,102 @@
+import json
+
 import requests
+import pprint
+
 
 class BaseRequest:
-   def __init__(self, base_url):
-       self.base_url = base_url
-
-   def get(self, endpoint):
-       url = f"{self.base_url}/{endpoint}"
-       response = requests.get(url)
-       return response
-
-   def post(self, endpoint, data):
-       url = f"{self.base_url}/{endpoint}"
-       response = requests.post(url, json=data)
-       return response
+    def init(self, base_url):
+        self.base_url = base_url
 
 
-base_url = "https://petstore.swagger.io"
-api = BaseRequest(base_url)
+    def _request(self, url, request_type, data=None, expected_error=False):
+        stop_flag = False
+        while not stop_flag:
+            if request_type == 'GET':
+                response = requests.get(url)
+            elif request_type == 'POST':
+                response = requests.post(url, json=data)
+            elif request_type == 'PUT':
+                response = requests.put(url, json=data)
+            else:
+                response = requests.delete(url)
 
- 
+            if not expected_error and response.status_code == 200:
+                stop_flag = True
+            elif expected_error:
+                stop_flag = True
+        return response
+
+    def get(self, endpoint, endpoint_id, expected_error=True):
+        url = f'{self.base_url}/{endpoint}/{endpoint_id}'
+        response = self._request(url, 'GET', expected_error=expected_error)
+        return response.json()
+
+    def post(self, endpoint, endpoint_id='', body=None):
+        url = f'{self.base_url}/{endpoint}/{endpoint_id}'
+        response = self._request(url, 'POST', data=body, expected_error=True)
+        return response.json()
+
+    def delete(self, endpoint, endpoint_id=''):
+        url = f'{self.base_url}/{endpoint}/{endpoint_id}'
+        response = self._request(url, 'DELETE')
+        return response.json()['message']
+
+    def put(self, endpoint, endpoint_id, body):
+        url = f'{self.base_url}/{endpoint}/{endpoint_id}'
+        response = self._request(url, 'PUT', data=body)
+        return response.json()['message']
 
 
-user_id = 1
-response = api.get(f"user/{user_id}")
-print(response.json())
+BASE_URL_PETSTORE = 'https://petstore.swagger.io/v2'
+base_request = BaseRequest(BASE_URL_PETSTORE)
 
- 
+data = {
+    "id": 3,
+    "petId": 1,
+    "quantity": 1,
+    "shipDate": "2023-12-27T18:25:17.448Z",
+    "status": "placed",
+    "complete": False
+}
 
+user_data = {
+    'id': 2,
+    'username': "AK",
+    'firstName': "Artem",
+    'lastName': "Kamaldinov",
+    'email': "artemkamaldinov@mail.ru",
+    'password': "Artem12345",
+    'phone': "912132132123",
+    'userStatus': 0
+  }
 
-new_user_data = {"username": "example_user", "email": "example@example.com"}
-response = api.post("user", new_user_data)
-print(response.json())
+#Добавлениу заказа
+# push_order = base_request.post('store', 'order', data) 
 
+#Удаление заказа
+# base_request.delete('store/order', 3) 
 
+#получение информации о списке товаров
+# store_info = base_request.get('store', 'inventory') 
+# pprint.pprint(store_info)
 
-order_id = 1
-response = api.get(f"store/order/{order_id}")
-print(response.json())
+ #получение информации о заказе
+# result = base_request.get('store/order', 2)  
+# pprint.pprint(result)
 
+#Добавление пользователя
+# user = base_request.post('user', body=user_data) 
+# pprint.pprint(user)
 
-new_order_data = {"petId": 1, "quantity": 1, "status": "placed"}
-response = api.post("store/order", new_order_data)
-print(response.json())
+#Изменение пользователя
+# user = base_request.put('user', 'AK', user_data)
+# pprint.pprint(user)
+
+#Получение информации о пользователе
+# user2 = base_request.get('user', 'AK')
+# pprint.pprint(user2)
+
+#Удаление пользователя
+# delete = base_request.delete('user', 'AK')
+# print(delete)
